b96eb117a43824f2c8e4791f947aa5b7eb46b792
junaid <junnujamshed@gmail.com>
2021-07-19 20:16:03 +0530

    assignment 1 done

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./style.css";
+import Button from "./Button";
+// import { Color } from 'jest-matcher-utils/node_modules/chalk'
+
+const App = () => {
+  return (
+    <div className="container">
+      <h1 style={{ textAlign: "center", color: "blue" }}>Hello World</h1>
+      <h2>@Junaid</h2>
+    </div>
+  );
+};
+
+export default App;
